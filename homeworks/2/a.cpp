7d94082e6f253541c98d0d147ccf1172db9233ff
Matvey Fedorov <mfgolden@yandex.ru>
2025-04-18 23:01:41 +0300

    1st version of A in hw2

--- homeworks/2/a.cpp
+++ homeworks/2/a.cpp
@@ -4,8 +4,28 @@
 using namespace std;
 
 typedef struct {
-    int lChild, rChild, value, mx;
-} Quadro;
+    int lChild, rChild, value, mxNum, lBorder, rBorder;
+} node;
+
+void dfs (vector<node> tree, int idx, int &ansMx, int &ansMxNum, pair<int, int> el) {
+    // cout << idx << " " << tree[idx].value << " " << ansMx << endl;
+    if (tree[idx].lChild != -1) {
+        if (tree[idx].rBorder < el.first || tree[idx].lBorder > el.second) {
+            return;
+        } else if (tree[idx].lBorder >= el.first && tree[idx].rBorder <= el.second) {
+            // cout << "i'm here ";
+            if (ansMx < tree[idx].value) {
+                ansMx = tree[idx].value;
+                ansMxNum = tree[idx].mxNum;
+            } else if (ansMx == tree[idx].value) {
+                ansMxNum += tree[idx].mxNum;
+            }
+        } else {
+            dfs(tree, tree[idx].lChild, ansMx, ansMxNum, el);
+            dfs(tree, tree[idx].rChild, ansMx, ansMxNum, el);
+        }
+    }
+}
 
 int main (void) {
     int N, K;    
@@ -14,10 +34,12 @@ int main (void) {
     int Npow = 1;
     for (auto i = 0; Npow < N; i++) Npow = 1 << i;
 
-    vector<Quadro> tree (2 * Npow - 1, {-1, -1, -1, -1});
+    vector<node> tree (2 * Npow - 1, {-1, -1, -1, 0, -1, -1});
 
+    int buf;
     for (auto i = 0; i < N; i++) {
-        cin >> tree[Npow - 1 + i].value;
+        cin >> buf;
+        tree[Npow - 1 + i] = {-1, -1, buf, 1, i, i};
     }
 
     cin >> K;
@@ -30,8 +52,37 @@ int main (void) {
         segments[i].second = bufR - 1;
     }
 
-    for (auto el : tree) cout << el.value << " ";
-    cout << endl;
+    int l, r, val, mxNum;
+    for (int i = Npow - 2; i >= 0; i--) {
+        l = 2*i + 1;
+        r = 2*i + 2;
+        val = max(tree[l].value, tree[r].value);
+        if (tree[l].value == tree[r].value) {
+            mxNum = tree[l].mxNum + tree[r].mxNum;
+        } else {
+            mxNum = (tree[l].value > tree[r].value) ? tree[l].mxNum : tree[r].mxNum;
+        }
+        
+        tree[i] = {l, r, val, mxNum, tree[l].lBorder, (tree[r].rBorder == -1) ? N : tree[r].rBorder};
+    }
+
+    // for (int i = 0; i < 2 * Npow - 1; i++) {
+    //     if (log2(i + 1) == ceil(log2(i + 1))) {
+    //         cout << endl;
+    //     }
+    //     cout << tree[i].lChild << ">" << tree[i].lBorder << ">" << tree[i].value << "<" << tree[i].rBorder << "<" << tree[i].rChild << " ";
+    // }
+
+    int ansMx, ansMxNum;
+
+    for (auto el : segments) {
+        ansMx = -1;
+        ansMxNum = 0;
+
+        dfs(tree, 0, ansMx, ansMxNum, el);
+
+        cout << ansMx << " " << ansMxNum << endl;
+    }
 
     return 0;
 }
